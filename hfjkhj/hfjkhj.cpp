b8d5336b97da7b3b674a67ccf7338bda9470a6b6
Top <Top@WIN-43S5BTL8AGG>
2024-10-10 11:39:44 +0500

    Добавьте файлы проекта.

---
+++ hfjkhj/hfjkhj.cpp
@@ -0,0 +1,57 @@
+﻿#include<iostream>
+//Задания 1
+//int main()
+//{
+//	int  Distancesa, time, speed;
+//	setlocale(LC_ALL, "ru");
+//	std::cout << "Введите растояния: "; 
+//	std::cin >> Distancesa;
+//
+//	std::cout << "Введите время: ";
+//	std::cin >> time;
+//
+//	std::cout << "Скорость равна: ";
+//	speed = Distancesa / time;
+//	std::cout << speed;
+//	
+//}
+
+//Задания 2
+//int main()
+//{
+//	int Start_time, End_time, watch, minutes, seconds;
+//	setlocale(LC_ALL, "ru");
+//	std::cout << "Введите время начало использования скутера, Введите часы, минуты, секунды: ";
+//	std::cin >> Start_time;
+//
+//	std::cin >> watch;
+//	std::cin >> minutes;
+//	std::cin >> seconds; 
+//
+//
+//
+//	
+//	std::cout << "Введмие время конец использования скутера Введите часы, минуты, секунды: ";
+//	std::cin >> End_time;
+//
+//	std::cin >> watch;
+//	std::cin >> minutes;
+//	std::cin >> seconds;
+//
+//	std::cout << "Стоимость поездки: ";
+//	minutes = minutes * 2;
+//	std::cout << minutes;
+//	std::cout << " Гривны";
+//}
+
+//Задания 3
+//int main()
+//{
+//	int distances, Consumption;
+//	setlocale(LC_ALL, "ru");
+//	std::cout << "Введите растояния: ";
+//	std::cin >> distances;
+//
+//	std::cout << "Расход: "
+//	std::cin >> Consumption ;  
+//}
